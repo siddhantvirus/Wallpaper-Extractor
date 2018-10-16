ddbb9129dd7b563ea562e243944e8bf1ba79f8b6
siddhantvirus <34143846+siddhantvirus@users.noreply.github.com>
2018-10-16 13:46:50 +0530

    Update walls.bat

--- walls.bat
+++ walls.bat
@@ -1,6 +1,6 @@
 ECHO ON
 :: lET US START.
 CD %HOMEPATH%/Desktop
-robocopy %LOCALAPPDATA%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets Wallpapers *.*  /min:100000
+robocopy "%LOCALAPPDATA%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" Wallpapers *.*  /min:100000
 cd Wallpapers
 ren *.* *.jpg
