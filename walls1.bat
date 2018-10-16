8fdf3cb835349a3af6289c8a67480272ce206973
siddhantvirus <34143846+siddhantvirus@users.noreply.github.com>
2018-10-16 13:40:06 +0530

    Solves Issue #1
    Enclosing the path between "" ensures that robocopy does not create issues for path names with spaces. 
    This is specifically useful when their are spaces in Usernames

--- walls1.bat
+++ walls1.bat
@@ -2,7 +2,7 @@
 echo lET US START.
 pause
 CD %HOMEPATH%/Desktop
-robocopy %LOCALAPPDATA%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets Wallpapers *.*  /min:100000
+robocopy "%LOCALAPPDATA%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" Wallpapers *.*  /min:100000
 cd Wallpapers
 ren *.* *.jpg
 echo done :)
